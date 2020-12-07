531031b93b9911518515143e046b2c8ed7f13032
Xavier Valls Pla <xaviervallspla@gmail.com>
2020-12-07 11:36:35 +0100

    implement part two (search of three summing 2020)

--- 2020/day01.py
+++ 2020/day01.py
@@ -1,33 +1,46 @@
 
-def orderFile(infile):
+def findTwoSum(infile, sum):
     with open(infile, "r") as f:
         lines = list(map(int, f))
         values = sorted(lines)
     lowit = 0
     topit = len(values)-1
-    while values[lowit]+values[topit] > 2020:
+    while values[lowit]+values[topit] > sum and topit>0:
         topit -= 1
     while lowit < topit:
         curSum = values[lowit]+values[topit]
-        if curSum == 2020:
+        if curSum == sum:
             return values[lowit]*values[topit]
-        if curSum < 2020:
+        if curSum < sum:
             lowit += 1
             topit += 1
         else:
             topit -= 1
     return -1
 
-def memoization(infile):
-    seen = {}
+
+def memoizationFindTwoSum(infile, sum):
+    seen = {}  # for O(1) lookup
     with open(infile, "r") as f:
         for line in f:
             n = int(line)
-            if (2020-n) in seen:
-                return n * (2020-n)
+            if (sum-n) in seen:
+                return n * (sum-n)
             seen[n] = True
     return -1
 
+
+def findThreeSum(infile, sum):
+    with open(infile, "r") as f:
+        values = list(map(int, f))
+    for n in values:
+        m = findTwoSum(infile, sum-n)
+        m = memoizationFindTwoSum(infile, sum-n)
+        if m>0: break
+    return n*m if m > 0 else -1
+
+
 if __name__ == '__main__':
-    print(orderFile("inputs/day01.txt"))
-    print(memoization("inputs/day01.txt"))
+    print(findTwoSum("inputs/day01.txt", 2020))
+    print(memoizationFindTwoSum("inputs/day01.txt", 2020))
+    print(findThreeSum("inputs/day01.txt", 2020))
